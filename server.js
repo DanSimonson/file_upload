b644907110c2f37484de1f18b9e1410a65d007ec
daniel simonson <dansimonson@mariposaweb.net>
2022-11-30 18:47:19 -0600

    change

--- server.js
+++ server.js
@@ -6,18 +6,18 @@ const app = express();
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//const __dirname = path.resolve();
+const __dirname = path.resolve();
 // Handle GET requests to /express_backend route
 app.get("/express_backend", (req, res) => {
   res.send({ message: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
 });
-//path.resolve
+// path.resolve
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 //..
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
 
 const PORT = process.env.PORT || 5001;
